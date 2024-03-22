0ece82217dc2b7ca3356cc0dc69f35e74b85aadf
= <rk8466995@gmail.com>
2024-03-22 16:01:22 +0500

    This is simple calculator

---
+++ main.ts
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  {
+    name: "num1",
+    type: "number",
+    message: "Please Enter Your First Digit: ",
+  },
+  {
+    name: "num2",
+    type: "number",
+    message: "Please Enter Your Second Digit: ",
+  },
+  {
+    name: "operator",
+    type: "list",
+    choices: [
+      "Addition",
+      "Subtraction",
+      "Division",
+      "Multiplication",
+      "Modulus",
+      "Exponent",
+    ],
+    message: "Please Select Your Operator: ",
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log(answer.num1 + answer.num2);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.num1 - answer.num2);
+} else if (answer.operator === "Division") {
+  console.log(answer.num1 / answer.num2);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.num1 * answer.num2);
+} else if (answer.operator === "Modulus") {
+  console.log(answer.num1 % answer.num2);
+} else if (answer.operator === "Exponent") {
+  console.log(answer.num1 ** answer.num2);
+} else {
+  console.log("Invalid Attempt, Please Try Again Later");
+}
